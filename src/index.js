085c353ebe4edaeb7601c6968f74155a229f233c
TeeL18 <torrence_95@hotmail.com>
2025-01-26 16:45:37 +0000

    Added basic Java handling and type writer handling

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
+function generatePoem(event) {
+  event.preventDefault();
 
+  new Typewriter("#poem", {
+    strings: "beautiful flowers",
+    autoStart: true,
+    delay: 75,
+    cursor: "",
+  });
+}
+
+let poemFormElement = document.querySelector("#poem-form");
+poemFormElement.addEventListener("submit", generatePoem);
