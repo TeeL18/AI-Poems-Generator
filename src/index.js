2e2386927cc81a9206317d1914db505451c5e538
TeeL18 <torrence_95@hotmail.com>
2025-01-26 18:33:41 +0000

    Added axios and type writer links as well as API intergration

--- src/index.js
+++ src/index.js
@@ -1,12 +1,23 @@
+function displayPoem(response) {
+  new Typewriter("#poem", {
+    strings: response.data.answer,
+    autoStart: true,
+    delay: 50,
+    cursor: "",
+  });
+}
+
 function generatePoem(event) {
   event.preventDefault();
 
-  new Typewriter("#poem", {
-    strings: "beautiful flowers",
-    autoStart: true,
-    delay: 75,
-    cursor: "",
-  });
+  let instructionsInput = document.querySelector("#user-instructions");
+  let apiKey = "oe47bb3454cd0f1af7dfdf430teb7c08";
+  let prompt = `Generate a poem about ${instructionsInput.value}`;
+  let context =
+    "You are an intelligent AI assistant that can provide nice and short answers to anything that has been asked. Do not write long poems.";
+  let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  axios.get(apiURL).then(displayPoem);
 }
 
 let poemFormElement = document.querySelector("#poem-form");
